38ed4fde1131e80a93e8202dad2d0d3e869f3fa7
Ilya Kravchenko <ilyak475@gmail.com>
2025-07-07 16:43:23 +0300

    Updste admin zone. Add DJDT

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,8 +1,43 @@
 from django.contrib import admin
 from .models import Category, IceCream, Topping, Wrapper
 
+admin.site.empty_value_display = 'Не задано'
 
-admin.site.register(Category)
-admin.site.register(IceCream)
+
+class IceCeamInline(admin.StackedInline):
+    model = IceCream
+    extra = 0
+    filter_horizontal = ('toppings',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCeamInline,
+    )
+    list_display = ('title',)
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    filter_horizontal = ('toppings',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(IceCream, IceCreamAdmin)
 admin.site.register(Topping)
 admin.site.register(Wrapper)
--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -4,7 +4,8 @@ from core.models import PublishedModel
 
 
 class Category(PublishedModel):
-    title = models.CharField('Название', max_length=256)
+    title = models.CharField('Название', max_length=256, help_text='Уникальное'
+                             ' название обёртки, не более 256 символов')
     slug = models.SlugField('Слаг', max_length=64, unique=True)
     output_order = models.PositiveSmallIntegerField('Порядок отображения',
                                                     default=100)
@@ -57,8 +58,10 @@ class IceCream(PublishedModel):
         related_name='ice_creams',
         verbose_name='Категория'
     )
-    toppings = models.ManyToManyField(Topping, verbose_name='Топпинг')
-    is_on_main = models.BooleanField('На главную', default=False)
+    toppings = models.ManyToManyField(
+        Topping, verbose_name='Топпинг')
+    is_on_main = models.BooleanField(
+        'На главную', default=False)
 
     class Meta:
         verbose_name = 'мороженое'
