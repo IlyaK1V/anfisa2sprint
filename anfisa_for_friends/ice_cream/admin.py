433e923f2698bf97deafbd06e6f538b008fef96e
Ilya Kravchenko <ilyak475@gmail.com>
2025-07-07 10:49:14 +0300

    Create admin zone

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, IceCream, Topping, Wrapper
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(IceCream)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
